643cd3da059f92ac0a85676cf7ed3841464ad202
habrimhdz <habrimhdz@gmail.com>
2025-07-01 12:45:52 -0600

    Finished the magic number exercise3 with the stretch challenges properly.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "yes";
+        while (response == "yes")
+        {
+            Random randomNumber = new Random();
+            int number = randomNumber.Next(1, 100);
+            Console.WriteLine($"Magic Number: {number}");
+
+            Console.WriteLine("Guess the Magic Number (1-100): ");
+            string userNumber = Console.ReadLine();
+            int userGuess = int.Parse(userNumber);
+            int attempts = 1;
+
+            while (userGuess != number)
+            {
+                attempts++;
+                if (userGuess > number)
+                {
+                    Console.WriteLine("Try Lower: ");
+                    userNumber = Console.ReadLine();
+                    userGuess = int.Parse(userNumber);
+                }
+                else if (userGuess < number)
+                {
+                    Console.WriteLine("Try Higher: ");
+                    userNumber = Console.ReadLine();
+                    userGuess = int.Parse(userNumber);
+                }
+
+            }
+            Console.WriteLine($"That's correct! You guessed the Magic Number {number} in {attempts} tries!");
+            Console.WriteLine("Do you want to play again? (type yes, otherwise the game will end) ");
+            response = Console.ReadLine();
+        }
+
+
+
+
+
+
+
     }
 }
