7998d8c52d198e94cfb47943431fa6d984371ef5
habrimhdz <habrimhdz@gmail.com>
2025-07-01 12:11:10 -0600

    Finished the letter grading Exercise2 correctly.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? (1-100) ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (gradePercentage < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter} ");
+
     }
-}
+} 
