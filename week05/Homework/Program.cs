8f86741bd3e30402b2b793c76195e4d8e74959d0
habrimhdz <habrimhdz@gmail.com>
2025-08-11 11:52:13 -0600

    Finished Homework Assignment Week05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("John Doe", "Doing my best");
+        Console.WriteLine(assignment1.getSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Bingus", "Bingulus", "Section 5.2", "Problems 1-10");
+        Console.WriteLine(mathAssignment1.getSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Sun Tzu", "The Art of War", "Analysis of Strategy");
+        Console.WriteLine(writingAssignment1.getSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
