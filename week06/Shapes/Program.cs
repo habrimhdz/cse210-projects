ae8f5fc243dd746520036f3dd101395f778de8e9
habrimhdz <habrimhdz@gmail.com>
2025-08-12 16:44:20 -0600

    Finished Shapes Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Square Color: {square.GetColor()}, Area: {square.GetArea()}");
+
+        Circle circle = new Circle("Blue", 3);
+        Console.WriteLine($"Circle Color: {circle.GetColor()}, Area: {circle.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Green", 4, 6);
+        Console.WriteLine($"Rectangle Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape> { square, circle, rectangle };
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
